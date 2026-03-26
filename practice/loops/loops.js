04b4c7a22c93c9be30499075ec68d4a523403a7b
Carlos tobar <carlosayalatobar91@gmail.com>
2026-03-26 12:47:44 -0400

    i failed

--- practice/loops/loops.js
+++ practice/loops/loops.js
@@ -2,10 +2,19 @@ let canvas = document.getElementById("canvas1");
 let ctx = canvas.getContext("2d");
 
 //fill in code for canvas1 here
-
+for(let x=25; x<800; x+=100){
+	ctx.fillRect(x,0,50,20);
+}
 canvas = document.getElementById("canvas2");
 ctx = canvas.getContext("2d");
+for(let y=0; y<=300; y+=150){
+	ctx.fillRect(0,y,300,200);
 
+}
+for(let x=25; x<800; x+=100){
+	ctx.fillRect(x,0,50,20);
+}
+if
 //fill in code for canvas2 here
 
 canvas = document.getElementById("canvas3");
